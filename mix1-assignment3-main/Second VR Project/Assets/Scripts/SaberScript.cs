6d1f20a9a80de8d1855fcfa9644c3d08bf8ae789
DanielaGarciaK <danniela.gk@gmail.com>
2023-10-07 13:21:09 +0200

    move to git

---
+++ mix1-assignment3-main/Second VR Project/Assets/Scripts/SaberScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SaberScript : MonoBehaviour
+{
+    private Vector3 previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit) 
+                                                                           )
+        {
+           if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130 && hit.collider.CompareTag(tag))
+           {
+                Destroy(hit.transform.gameObject);
+           }
+        }
+        previousPos = transform.position;
+    }
+
+    
+}
